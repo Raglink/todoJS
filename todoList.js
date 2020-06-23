55196862252ab41e9df027bf57cc095455d153dd
Vincent Fagoaga <fago40@gmail.com>
2020-06-23 22:00:03 +0200

    add feature to display task done

--- todoList.js
+++ todoList.js
@@ -1,23 +1,67 @@
-console.info("script loaded");
+// global variables
+let todoListArray = [];
+
+// define functions
+const clearToDoList = () => {
+  if (document.getElementById("todo-list")) {
+    if (document.getElementById("todo-list").childNodes.length) {
+      let oldList = document.getElementById("todo-list");
+      for (
+        let i = document.getElementById("todo-list").childNodes.length - 1;
+        i >= 0;
+        i--
+      ) {
+        oldList.removeChild(oldList.childNodes[i]);
+      }
+    }
+  }
+};
+
+// TO DO now
+const displayTaskDone = (selectedNode) => {
+  console.log(selectedNode);
+  if (selectedNode) {
+    if (selectedNode.classList.value !== "task-done") {
+      selectedNode.classList.add("task-done");
+      console.log("add task-done");
+    } else {
+      console.log("already task-done");
+      selectedNode.classList.remove("task-done");
+    }
+  }
+};
+
+const displayToDoList = () => {
+  clearToDoList();
+  if (todoListArray) {
+    for (let i = 0; i < todoListArray.length; i++) {
+      let tag = document.createElement("li");
+      let tagContent = document.createTextNode(todoListArray[i]);
+      tag.onclick = function () {
+        displayTaskDone(tag);
+      };
+      tag.appendChild(tagContent);
+      let element = document.getElementById("todo-list");
+      element.appendChild(tag);
+    }
+  } else {
+    console.error("Error todoListArray in displayToDoList ");
+  }
+};
 
 const addToTodo = () => {
-  console.info("clicked");
   // select new task
   if (document.getElementById("new-task").value.length) {
-    console.log("length OK");
-    // add new task in todo list
-    // create a new node
-    let tag = document.createElement("li");
-    let tagContent = document.createTextNode(
-      document.getElementById("new-task").value
-    );
-    tag.appendChild(tagContent);
-    let element = document.getElementById("todo-list");
-    element.appendChild(tag);
+    todoListArray.push(document.getElementById("new-task").value);
+    console.log("todoListArray ", todoListArray);
+    displayToDoList();
   } else {
     alert("Merci de saisir une nouvelle tâche");
     //console.error("problem with new-task length");
   }
-
-  // create a new node
 };
+
+// TODO global
+// add onclick  class (  text-decoration: line-through;) or not
+// delete
+//
