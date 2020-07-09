1bb83de09485f571ab623971a36c1d0b06ea9c5d
Vincent Fagoaga <fago40@gmail.com>
2020-07-09 17:22:30 +0200

    clean old code + task sort OK + display task done OK

--- README.md
+++ README.md
@@ -3,10 +3,8 @@
 ## todo
 
 <ul>
-    <li>Fix -  CSS on task done is missing</li>
     <li>Fix -  CSS cross position for safari and chrome</li>
     <li>Feat - CSS desktop and mobile</li>
-    <li>Feat - Sort tasks done at last</li>
     <li>Feat - Save todo list in local storage</li>
     <li>Feat - Edit task</li>
     <li>Feat - submit input on enter </li>
--- todoList.js
+++ todoList.js
@@ -6,6 +6,20 @@ let List = function () {
 let todoList = new List();
 
 // Define functions
+List.prototype.sort = function () {
+  function compare(a, b) {
+    const valueA = a.isDone;
+    const valueB = b.isDone;
+    let comparison = 0;
+    if (valueA > valueB) {
+      comparison = 1;
+    } else if (valueA < valueB) {
+      comparison = -1;
+    }
+    return comparison;
+  }
+  this.list = this.list.sort(compare);
+};
 List.prototype.display = function () {
   console.log("display", todoList);
 };
@@ -25,6 +39,7 @@ List.prototype.add = function (textFieldValue) {
   console.log("thisList", this.list);
   this.list.push({ value: textFieldValue, isDone: false });
   todoList.save();
+  todoList.sort();
   todoList.display();
 };
 
@@ -35,6 +50,7 @@ List.prototype.done = function (rank) {
   } else {
     this.list[rank].isDone = true;
   }
+  todoList.sort();
   todoList.display();
 };
 
@@ -42,33 +58,8 @@ List.prototype.remove = function (rank) {
   this.list.splice(rank, 1);
   todoList.display();
 };
-//test section -------->
-//todoList.save();
-// todoList.display();
-// todoList.load();
-// todoList.add("toto");
-// todoList.add("toto");
 
-let todoListArray = [];
-
-// define functions
-const updatelistCss = (nodeToUpdate, isDoneToogle) => {
-  console.log("nodeToUpdate", nodeToUpdate);
-  console.log("isDoneToogle", isDoneToogle);
-
-  if (nodeToUpdate && typeof isDoneToogle === "boolean") {
-    console.log("tototo ", isDoneToogle);
-    if (isDoneToogle) {
-      console.log("nodeToUpdate.classList ", nodeToUpdate.classList);
-      nodeToUpdate.classList.add("task-done");
-      console.log("nodeToUpdate.classList => ", nodeToUpdate.classList);
-    } else {
-      nodeToUpdate.classList.remove("task-done");
-    }
-  } else {
-    console.warn("problem with nodeToUpdate or isDoneToogle");
-  }
-};
+// Define display rules
 const clearToDoList = () => {
   if (document.getElementById("todo-list")) {
     if (document.getElementById("todo-list").childNodes.length) {
@@ -84,53 +75,8 @@ const clearToDoList = () => {
   }
 };
 
-const sortToDoList = () => {
-  function compare(a, b) {
-    const valueA = a.isDone;
-    const valueB = b.isDone;
-    let comparison = 0;
-    if (valueA > valueB) {
-      comparison = 1;
-    } else if (valueA < valueB) {
-      comparison = -1;
-    }
-    return comparison;
-  }
-  todoListArray = todoListArray.sort(compare);
-};
-
-const displayTaskDone = (selectedNode, rank) => {
-  if (selectedNode && rank >= 0) {
-    console.log(selectedNode);
-    if (todoListArray[rank]) {
-      console.log("here ", todoListArray[rank]);
-      console.log("todoListArray[rank].isDone ", todoListArray[rank].isDone);
-
-      if (todoListArray[rank].isDone == false) {
-        todoListArray[rank].isDone = true;
-      } else {
-        todoListArray[rank].isDone = false;
-      }
-      console.log("todoListArray[rank].isDone => ", todoListArray[rank].isDone);
-      updatelistCss(selectedNode, todoListArray[rank].isDone);
-      displayToDoList();
-    } else {
-      console.info("last clicked node is deleted");
-    }
-  } else {
-    console.warn(
-      "problem with selectedNode et rank ",
-      " selectedNode ",
-      selectedNode,
-      "rank ",
-      rank
-    );
-  }
-};
-
 const displayToDoList = () => {
   clearToDoList();
-  sortToDoList();
   if (todoList.list.length >= 0) {
     for (let i = 0; i < todoList.list.length; i++) {
       //cross
@@ -149,31 +95,29 @@ const displayToDoList = () => {
       let tagContent = document.createTextNode(todoList.list[i].value);
       tag.onclick = function () {
         todoList.done(i);
+        displayToDoList();
       };
 
       // update DOM element
       let element = document.getElementById("todo-list");
       tag.appendChild(cross);
       tag.appendChild(tagContent);
+      if (todoList.list[i].isDone) {
+        tag.classList.add("task-done");
+      }
       element.appendChild(tag);
     }
   } else {
-    console.error("Error todoListArray in displayToDoList ");
+    console.error("Error todoList in displayToDoList ");
   }
 };
 
 const addToTodo = () => {
   // select new task
   if (document.getElementById("new-task").value.length) {
-    todoListArray.push({
-      value: document.getElementById("new-task").value,
-      isDone: false,
-    });
-    console.log("todoListArray ", todoListArray);
-    displayToDoList();
     todoList.add(document.getElementById("new-task").value);
+    displayToDoList();
   } else {
     alert("Merci de saisir une nouvelle tâche");
   }
-  displayToDoList();
 };
